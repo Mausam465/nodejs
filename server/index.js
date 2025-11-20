081308dd214ecf39dda865c69f234fceedc86180
Mausam Kumari <mausamkumari9095@gmail.com>
2025-11-20 23:22:46 +0530

    new changes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const http= require('http');
+const fs=require('fs');
 const server=http.createServer((req,res)=>{
-    console.log("New request received");
-    res.end("Hello from server");
-    
-    
+    // console.log("New request received");
+    const log=`${Date.now()}: ${req.url} New request received\n`;
+    fs.appendFile('log.txt',log,(err,data)=>{
+        //  res.end("Hello from server again");
+        switch(req.url){
+            case '/': res.end('Home page');
+            break;
+            case '/about': res.end('I am mausam');
+            break;
+            default : res.end('404 not found');
+        }
+    });
 });
 server.listen(8000,()=> console.log("server started"));
