6766f36e62e77afc4fb5979d0ec53682bad43e6a
Mausam Kumari <mausamkumari9095@gmail.com>
2025-11-22 23:12:49 +0530

    JSON

--- 01_project/index.js
+++ 01_project/index.js
@@ -1,5 +1,10 @@
 const express=require('express');
+const users=require('./MOCK_DATA.json');
 const app=express();
 const port=8000;
+
+app.get('/users',(req,res)=>{
+    return res.json(users);
+})
 
 app.listen(port,()=>console.log('server started'));
