1af5c5d9ecb52605614978bcf87c420b2d3ca95c
Mausam Kumari <mausamkumari9095@gmail.com>
2025-11-23 17:26:55 +0530

    new changes

--- 01_project/index.js
+++ 01_project/index.js
@@ -3,8 +3,16 @@ const users=require('./MOCK_DATA.json');
 const app=express();
 const port=8000;
 
+app.get('/api/users',(req,res)=>{
+    return res.json(users);  
+});
 app.get('/users',(req,res)=>{
-    return res.json(users);
+    const html=`
+    <ul>
+    ${users.map((user)=> `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
 })
 
 app.listen(port,()=>console.log('server started'));
